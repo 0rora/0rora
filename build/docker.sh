31f8bbf7b243f7cfc3de66103652c3c35531355f
Jem Mawson <jem.mawson@gmail.com>
2018-09-16 21:06:56 +1000

    prepare dist before docker

--- build/docker.sh
+++ build/docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+sbt dist
 docker build -f build/Dockerfile -t luxe-app/luxe .
 docker tag luxe-app/luxe:$TRAVIS_TAG
 docker tag luxe-app/luxe:latest
