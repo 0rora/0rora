6ee1dcb37d70a0b16f6ac43f1565e2518a4cf0b5
Jem Mawson <jem.mawson@gmail.com>
2018-09-16 21:39:57 +1000

    change of dockerhub team name

--- build/docker.sh
+++ build/docker.sh
@@ -4,7 +4,7 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 sbt dist
 docker build \
     -f build/Dockerfile \
-    -t luxe-app/luxe:latest \
-    -t luxe-app/luxe:$TRAVIS_TAG \
+    -t luxeapp/luxe:latest \
+    -t luxeapp/luxe:$TRAVIS_TAG \
     .
-docker push luxe-app/luxe
+docker push luxeapp/luxe
