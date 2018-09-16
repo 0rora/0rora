6c4290ae7f2fbda8135cb550ddee02ca5b87cd7f
Jem Mawson <jem.mawson@gmail.com>
2018-09-16 21:14:57 +1000

    upgrade to java 11 & fix docker tag command

--- build/docker.sh
+++ build/docker.sh
@@ -2,7 +2,8 @@
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 sbt dist
-docker build -f build/Dockerfile -t luxe-app/luxe .
-docker tag luxe-app/luxe:$TRAVIS_TAG
-docker tag luxe-app/luxe:latest
+docker build
+    -f build/Dockerfile \
+    -t luxe-app/luxe:latest luxe-app/luxe:$TRAVIS_TAG \
+    .
 docker push luxe-app/luxe
