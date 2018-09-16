08248d095d0f0aa995f45d0d0c62d15ddb56a5c9
Jem Mawson <jem.mawson@gmail.com>
2018-09-16 21:25:18 +1000

    separate "t" flags

--- build/docker.sh
+++ build/docker.sh
@@ -4,6 +4,7 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 sbt dist
 docker build \
     -f build/Dockerfile \
-    -t luxe-app/luxe:latest luxe-app/luxe:$TRAVIS_TAG \
+    -t luxe-app/luxe:latest \
+    -t luxe-app/luxe:$TRAVIS_TAG \
     .
 docker push luxe-app/luxe
