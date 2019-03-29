e6002080f6656fbc2955f95cb8e95535a39b209d
Jem Mawson <jem.mawson@gmail.com>
2019-03-29 21:17:50 +1000

    add custom token to demo file

--- app/controllers/DemoSiteAssetsController.scala
+++ app/controllers/DemoSiteAssetsController.scala
@@ -4,9 +4,10 @@ import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter.ISO_OFFSET_DATE_TIME
 
 import akka.util.ByteString
-import controllers.actions.AuthenticatedUserAction
 import javax.inject.{Inject, Singleton}
 import models.AppConfig
+import org.pac4j.core.profile.CommonProfile
+import org.pac4j.play.scala.{Security, SecurityComponents}
 import play.api.http.HttpEntity.Strict
 import play.api.mvc._
 import stellar.sdk.KeyPair
@@ -14,14 +15,13 @@ import stellar.sdk.KeyPair
 import scala.util.Random
 
 @Singleton
-class DemoSiteAssetsController @Inject()(cc: ControllerComponents,
-                                         config: AppConfig,
-                                         authenticatedUserAction: AuthenticatedUserAction) extends AbstractController(cc) {
+class DemoSiteAssetsController @Inject()(val controllerComponents: SecurityComponents,
+                                         config: AppConfig) extends BaseController with Security[CommonProfile] {
 
   private def format(zdt: ZonedDateTime): String = ISO_OFFSET_DATE_TIME.format(zdt)
   private val accounts: Seq[KeyPair] = config.accounts.values.toSeq
 
-  def paymentsCSV: Action[AnyContent] = authenticatedUserAction { implicit request: Request[AnyContent] =>
+  def paymentsCSV: Action[AnyContent] =Secure("FormClient") { implicit req =>
     val now = ZonedDateTime.now()
     Ok.sendEntity(Strict(
       ByteString(
@@ -29,6 +29,7 @@ class DemoSiteAssetsController @Inject()(cc: ControllerComponents,
            |$csvAccounts,XLM,,5000,${format(now.minusMinutes(30))}
            |$csvAccounts,XLM,,5000,${format(now)}
            |$csvAccounts,XLM,,5000,${format(now.plusMinutes(1))}
+           |GCCGEJC45TVQUVQ3HH6JUFD4Q3NO3XYTMWWKUVJP6Z3MR7D3YLOB6XXC,GBFEQSR3ZDWANTL4BFKVUS4A73HBFRSBLSBJE5POKICVT2OGDK2AVLCY,BananaMan,GCCGEJC45TVQUVQ3HH6JUFD4Q3NO3XYTMWWKUVJP6Z3MR7D3YLOB6XXC,5000,${format(now)}
         """.stripMargin), Some("application/x-download")
     ))
   }
