bd02017ee580c52effca5b6015e586b91a3841ee
Jem Mawson <jem.mawson@gmail.com>
2019-04-01 21:37:43 +1000

    revert to form of module that pac4j can deal with

--- app/modules/SecurityModule.scala
+++ app/modules/SecurityModule.scala
@@ -1,38 +1,40 @@
 package modules
 
-import com.google.inject.Provides
+import com.google.inject.{AbstractModule, Provides}
 import com.zaxxer.hikari.HikariDataSource
 import models.InvalidConfig
 import org.apache.shiro.authc.credential.DefaultPasswordService
 import org.pac4j.core.client.Clients
 import org.pac4j.core.config.Config
 import org.pac4j.core.credentials.password.ShiroPasswordEncoder
+import org.pac4j.core.profile.CommonProfile
 import org.pac4j.http.client.indirect.FormClient
 import org.pac4j.play.LogoutController
 import org.pac4j.play.http.PlayHttpActionAdapter
-import org.pac4j.play.scala.{DefaultSecurityComponents, SecurityComponents}
+import org.pac4j.play.scala.{DefaultSecurityComponents, Pac4jScalaTemplateHelper, SecurityComponents}
 import org.pac4j.play.store.{DataEncrypter, PlayCookieSessionStore, PlaySessionStore, ShiroAesDataEncrypter}
 import org.pac4j.sql.profile.service.DbProfileService
-import play.api.inject.{Binding, Module}
 import play.api.{Configuration, Environment}
 
-class SecurityModule extends Module {
+class SecurityModule(environment: Environment, configuration: Configuration) extends AbstractModule {
 
-  def bindings(env: Environment, config: Configuration): Seq[Binding[_]] = {
-    val playSessionStore = new PlayCookieSessionStore(provideEncrypter(config))
+  // $COVERAGE-OFF$
+  override def configure(): Unit = {
+    val playSessionStore = new PlayCookieSessionStore(provideEncrypter)
 
+    bind(classOf[PlaySessionStore]).toInstance(playSessionStore)
+    bind(classOf[SecurityComponents]).to(classOf[DefaultSecurityComponents])
+    bind(classOf[Pac4jScalaTemplateHelper[CommonProfile]])
+
+    // logout
     val logoutController = new LogoutController()
     logoutController.setDefaultUrl("/")
-
-    Seq(
-      bind[PlaySessionStore].toInstance(playSessionStore),
-      bind[SecurityComponents].to(classOf[DefaultSecurityComponents]),
-      bind[LogoutController].toInstance(logoutController)
-    )
+    bind(classOf[LogoutController]).toInstance(logoutController)
   }
+  // $COVERAGE-ON$
 
   @Provides
-  def provideEncrypter(configuration: Configuration): DataEncrypter = {
+  def provideEncrypter: DataEncrypter = {
     val secretKey = configuration.get[String]("play.http.secret.key")
     if (secretKey.length < 16) throw InvalidConfig("Value for play.http.secret.key is too short. Must be at least 16 characters.")
     val sKey = secretKey.take(16)
@@ -40,7 +42,7 @@ class SecurityModule extends Module {
   }
 
   @Provides
-  def provideAuthenticator(configuration: Configuration): DbProfileService = {
+  def provideAuthenticator: DbProfileService = {
     val ds = new HikariDataSource()
     ds.setDriverClassName(configuration.get[String]("db.default.driver"))
     ds.setJdbcUrl(configuration.get[String]("db.default.url"))
